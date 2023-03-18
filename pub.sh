8f1faeb07b597541bbcdabbf1cd624e0a7adfb23
inomoto <medalhkr@gmail.com>
2023-03-18 09:24:55 +0900

    first

---
+++ pub.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sas=$(az iot hub generate-sas-token --cs "$CS" | jq -r .sas)
+
+hub=sbox-iot
+device=wsl2
+
+mosquitto_pub -d -q 1 \
+  --capath /etc/ssl/certs/ \
+  -V mqttv311 \
+  -p 8883 \
+  -h ${hub}.azure-devices.net \
+  -i $device \
+  -u "${hub}.azure-devices.net/$device/api-version=2016-11-14" \
+  -P "$sas" \
+  -t "devices/$device/messages/events/" \
+  -m '{"v":"hi"}'
